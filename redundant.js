1d97c11b65267065a0b025b35df17f5dd2f1ab19
rohitk2 <rohit98.kumar@gmail.com>
2018-08-05 11:48:00 -0700

    Add files via upload

---
+++ redundant.js
@@ -0,0 +1,57 @@
+//Overview         
+var divElement = document.getElementById('viz1532254572584');                    
+var vizElement = divElement.getElementsByTagName('object')[0];                    
+vizElement.style.width='100%';
+vizElement.style.height=(divElement.offsetWidth*0.75)+'px';                    
+var scriptElement = document.createElement('script');                   
+scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    
+vizElement.parentNode.insertBefore(scriptElement, vizElement);   
+             
+//PPG
+var divElement_1 = document.getElementById('viz1532256517122');                    
+var vizElement_1 = divElement_1.getElementsByTagName('object')[0];                    
+vizElement_1.style.width='100%';
+vizElement_1.style.height=(divElement_1.offsetWidth*0.75)+'px';                    
+var scriptElement_1 = document.createElement('script');                    
+scriptElement_1.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                   
+vizElement_1.parentNode.insertBefore(scriptElement_1, vizElement_1);   
+//Points Given
+var divElement_2 = document.getElementById('viz1532255860345');                    
+var vizElement_2 = divElement_2.getElementsByTagName('object')[0];                    
+vizElement_2.style.width='100%';
+vizElement_2.style.height=(divElement_2.offsetWidth*0.75)+'px';                    
+var scriptElement_2 = document.createElement('script');                    
+scriptElement_2.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                   
+vizElement_2.parentNode.insertBefore(scriptElement_2, vizElement_2);   
+//Field Goal Percentage
+var divElement_3 = document.getElementById('viz1532255997928');                    
+var vizElement_3 = divElement_3.getElementsByTagName('object')[0];                    
+vizElement_3.style.width='100%';
+vizElement_3.style.height=(divElement_3.offsetWidth*0.75)+'px';                    
+var scriptElement_3 = document.createElement('script');                    
+scriptElement_3.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                   
+vizElement_3.parentNode.insertBefore(scriptElement_3, vizElement_3);         
+//Rebound Margin
+var divElement_4 = document.getElementById('viz1532256089044');                    
+var vizElement_4 = divElement_4.getElementsByTagName('object')[0];                    
+vizElement_4.style.width='100%';
+vizElement_4.style.height=(divElement_4.offsetWidth*0.75)+'px';                    
+var scriptElement_4 = document.createElement('script');                    
+scriptElement_4.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                   
+vizElement_4.parentNode.insertBefore(scriptElement_4, vizElement_4);  
+//Steals
+var divElement_5 = document.getElementById('viz1532256184426');                    
+var vizElement_5 = divElement_5.getElementsByTagName('object')[0];                    
+vizElement_5.style.width='100%';
+vizElement_5.style.height=(divElement_5.offsetWidth*0.75)+'px';                    
+var scriptElement_5 = document.createElement('script');                    
+scriptElement_5.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                   
+vizElement_5.parentNode.insertBefore(scriptElement_5, vizElement_5);  
+//Blocks
+var divElement_6 = document.getElementById('viz1532256327472');                    
+var vizElement_6 = divElement_6.getElementsByTagName('object')[0];                    
+vizElement_6.style.width='100%';
+vizElement_6.style.height=(divElement_6.offsetWidth*0.75)+'px';                    
+var scriptElement_6 = document.createElement('script');                    
+scriptElement_6.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                   
+vizElement_6.parentNode.insertBefore(scriptElement_6, vizElement_6);  
